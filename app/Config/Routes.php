10be0e6ccc1f558150b39f37c8425af1029c04ca
Henry Pauli <henry@mixict.nl>
2022-01-13 12:16:38 +0100

    Ticketshop does POST request

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,8 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/neworder', 'Webhook::neworder');
+#$routes->get('/neworder', 'Webhook::neworder');
+$routes->match(['get', 'post'], 'neworder', 'Webhook::neworder');
 
 /*
  * --------------------------------------------------------------------
